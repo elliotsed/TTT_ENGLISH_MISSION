b6c93a97b194300909fd873da9950d29c2ec0712
elliot <elliot.sedogbo@epitech.eu>
2023-12-11 11:15:24 +0100

    changement du footer

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -35,8 +35,11 @@ function App() {
             <p>Contactez-nous maintenant par WhatsApp pour démarrer un cours personnalisé, adapté à vos besoins et à votre disponibilié.</p>
 
             <div className="d-flex justify-content-center">
-              <a className="btn custom-color" href="https://wa.me/+22951003776/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20services.">
-                <Icon icon="logos:whatsapp-icon" width={30} /> Let's go
+              <a className="btn custom-color" href="https://wa.me/+22951003776/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
+                <Icon icon="logos:whatsapp-icon" width={30} /> Par Whatsapp
+              </a>
+              <a className="btn custom-color" href="mailto:tttmission@gmail.com">
+                <Icon icon="clarity:email-solid" width={30} className='me-2' /> Par E-mail
               </a>
             </div>
           </div>
--- ttt-webapp/src/components/Section1.js
+++ ttt-webapp/src/components/Section1.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../App.css';
-import { Icon } from '@iconify/react';
-
 
 
 function Section1() {
     return (
         <div className="container-fluid ff">
             <div className="row">
-                <div className="mt-3 text-center">
+                <div className="mt-4 text-center">
                     <div className="d-flex justify-content-center">
                         <h1>TRAINING THAT TRANSFORM</h1>
 
@@ -16,9 +14,6 @@ function Section1() {
                     <div className="d-flex justify-content-center">
                         <p>Vous apprendrez la manière la plus simple et la plus facile d'apprendre l'anglais grâce à TTT ENGLISH MISSION</p>
 
-                    </div>
-                    <div className=" d-flex justify-content-center d-sm-none d-md-none d-lg-none">
-                        <Icon icon="clarity:email-solid" width={25} className='me-2' /> tttmission@gmail.com
                     </div>
                     <div className="d-flex justify-content-center mt-2">
                         <button className="btn custom-color text-white">Inscription</button>
