878d5cf3564f72e0f0163b27c20eb16d8e4b315b
elliot <elliot.sedogbo@epitech.eu>
2023-12-11 11:37:33 +0100

    backup

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -36,8 +36,8 @@ function App() {
               <a className="btn custom-color" href="https://wa.me/+22994605100/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
                 <Icon icon="logos:whatsapp-icon" width={30} /> Par Whatsapp
               </a>
-              <a className="btn custom-color" href="mailto:tttmission@gmail.com">
-                <Icon icon="clarity:email-solid" width={30} className='me-2' /> Par E-mail
+              <a className="btn custom-color me-2" href="mailto:tttmission@gmail.com">
+                <Icon icon="clarity:email-solid" width={30} /> Par E-mail
               </a>
             </div>
           </div>
