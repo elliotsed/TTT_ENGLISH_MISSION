9d2a2026c16848e289275118d77fa19bcfcb5083
elliot <elliot.sedogbo@epitech.eu>
2023-12-29 21:17:54 +0100

    quelques modifs

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -30,13 +30,11 @@ function App() {
         <div className="row">
           <div className="mt-5 text-center">
             <h1>LET’S BUILD YOUR DREAMS</h1>
-            <p className="custom-size mt-3">Contactez Le Manager au <strong>+229 94605100</strong></p>
-            <p className="custom-size mt-3">OU</p>
-            <p className="custom-size">Contactez-nous directement par WhatsApp</p>
-
+            <p className="custom-size mt-3">Contact: <strong>+229 94605100</strong></p>
+            <p className="custom-size mt-3">E-mail: <strong>tttmission@gmail.com</strong></p>
             <div className="d-flex justify-content-center">
               <a className="btn custom-color" href="https://wa.me/+22969109929/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
-                <Icon icon="logos:whatsapp-icon" width={30} /> Let's go
+                <Icon icon="logos:whatsapp-icon" width={30} /> Whatsapp
               </a>
             </div>
           </div>
--- ttt-webapp/src/components/Section1.js
+++ ttt-webapp/src/components/Section1.js
@@ -9,11 +9,12 @@ function Section1() {
                 <div className="mt-4 text-center">
                     <div className="d-flex justify-content-center">
                         <h1>TRAININGS THAT TRANSFORM</h1>
-
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <p>Dépassez totalement votre peur de vous exprimer couramment en Anglais !</p>
                     </div>
                     <div className="d-flex justify-content-center">
                         <p>Découvrez ce que nous ferons avec vous avec passion tous les jours durant les prochains mois de l'année.</p>
-
                     </div>
                     <div className="d-flex justify-content-center">
                         <p>Adresse-Siège: Akassato, Goudron Kerekou, Rue de La Boulangerie Main D'or</p>
