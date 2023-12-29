d4c5317680bfc6ed02608d3f25545df1183ebd92
elliot <elliot.sedogbo@epitech.eu>
2023-12-29 21:20:33 +0100

    flem

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -30,8 +30,8 @@ function App() {
         <div className="row">
           <div className="mt-5 text-center">
             <h1>LET’S BUILD YOUR DREAMS</h1>
-            <p className="custom-size mt-3">Contact: <strong>+229 94605100</strong></p>
-            <p className="custom-size mt-3">E-mail: <strong>tttmission@gmail.com</strong></p>
+            <p className="custom-size mt-3">Contact: +229 94605100</p>
+            <p className="custom-size mt-3">E-mail: tttmission@gmail.com</p>
             <div className="d-flex justify-content-center">
               <a className="btn custom-color" href="https://wa.me/+22969109929/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
                 <Icon icon="logos:whatsapp-icon" width={30} /> Whatsapp
