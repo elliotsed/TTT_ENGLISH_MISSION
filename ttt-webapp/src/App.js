fe4e44241c31dfcf36e51839ac7731bdde180900
elliot <elliot.sedogbo@epitech.eu>
2024-01-19 18:30:55 +0100

    maintenance

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -29,14 +29,17 @@ function App() {
       <div className="container-fluid ff2" id="contact">
         <div className="row">
           <div className="mt-5 text-center">
-            <h1>LET’S BUILD YOUR DREAMS</h1>
-            <p className="custom-size mt-3">Contact: +229 69109929</p>
-            <p className="custom-size mt-3">E-mail: tttmission@gmail.com</p>
+            <h2 className="text-white">LET’S BUILD YOUR DREAMS</h2>
+            <p className="custom-size mt-3"><Icon icon="ion:location-sharp" color="white" width={35}/> Akassato, Goudron Kerekou, Rue de La Boulangerie Main D'or</p>
+            <p className="custom-size mt-3"><Icon icon="clarity:email-solid" color="white" width={35}/> tttmission@gmail.com</p>
+            <p className="custom-size mt-3"><Icon icon="material-symbols:call" color="white" width={35}/> +229 69109929</p>
+
             <div className="d-flex justify-content-center">
               <a className="btn custom-color" href="https://wa.me/+22969109929/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
                 <Icon icon="logos:whatsapp-icon" width={30} /> Whatsapp
               </a>
             </div>
+            
           </div>
         </div>
 
--- ttt-webapp/src/components/Section1.js
+++ ttt-webapp/src/components/Section1.js
@@ -11,16 +11,15 @@ function Section1() {
                         <h1>TRAININGS THAT TRANSFORM</h1>
                     </div>
                     <div className="d-flex justify-content-center">
-                        <p>Dépassez totalement votre peur de vous exprimer couramment en Anglais !</p>
+                        <p>Dépassez totalement votre peur de vous exprimer couramment en Anglais.</p>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <p>Démarrez maintenant un cours personnalisé, entièrement adapté à vos besoins et à votre disponibilité.</p>
                     </div>
                     <div className="d-flex justify-content-center">
                         <p>Découvrez ce que nous ferons avec vous avec passion tous les jours durant les prochains mois de l'année.</p>
                     </div>
-                    <div className="d-flex justify-content-center">
-                        <p>Adresse-Siège: Akassato, Goudron Kerekou, Rue de La Boulangerie Main D'or</p>
-                    </div>
-                   
-
+                    
                 </div>
             </div>
         </div>
