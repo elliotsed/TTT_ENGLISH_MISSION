231c0f07c526e8bfbb509ccd346689e0e26d1a53
Elliot <elliot.sedogbo@epitech.eu>
2024-10-06 17:20:49 +0100

    numero changé

--- ttt-webapp/src/App.js
+++ ttt-webapp/src/App.js
@@ -31,7 +31,7 @@ function App() {
             <h2 className="text-white">LET’S BUILD YOUR DREAMS</h2>
             <p className="custom-size mt-3"><Icon icon="ion:location-sharp" color="white" width={35}/> Akassato, Goudron Kerekou, Rue de La Boulangerie Main D'or</p>
             <p className="custom-size mt-3"><Icon icon="clarity:email-solid" color="white" width={35}/> tttmission@gmail.com</p>
-            <p className="custom-size mt-3"><Icon icon="material-symbols:call" color="white" width={35}/> +229 69109929</p>
+            <p className="custom-size mt-3"><Icon icon="material-symbols:call" color="white" width={35}/> +229 94605100</p>
 
             <div className="d-flex justify-content-center">
               <a className="btn custom-color" href="https://wa.me/+22969109929/?text=Bonjour%20!%20Je%20suis%20intéressé%20par%20vos%20formations.">
